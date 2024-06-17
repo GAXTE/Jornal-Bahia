01b081b551cd57c9891c4c43c250975c5ddd08f2
DanielVrs <danielvrsdev@gmail.com>
2024-06-17 16:38:53 -0300

    feat: init config variants and classes typograph

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
@@ -10,41 +11,87 @@ export default {
           lg: "1120px",
         },
       },
-      spacing: {
-        "19px": "19px",
-      },
       fontFamily: {
         primaryFont: ["Montserrat", "sans-serif"],
       },
-      fontSize: {
-        "14px": "14px",
-        "16px": "16px",
-        "18px": "18px",
-        "20px": "20px",
-        "24px": "24px",
-        "32px": "32px",
-        "48px": "48px",
-        "64px": "64px",
-      },
-      fontWeight: {
-        light: 300,
-        regular: 400,
-        medium: 600,
-        bold: 700,
-      },
-      lineHeight: {
-        "19.5px": "19.5px",
-        "32px": "32px",
-        "48px": "48px",
-        "64px": "64px",
-      },
-    },
-    colors: {
-      primaryRed: "#FF0000",
-      bodyColor: "#F3F3F3",
-      black: "#000000",
-      white: "#FFFFFF",
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".title-1": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "32px",
+          color: "var(--color-black)",
+          fontWeight: "800",
+          lineHeight: "39.01px",
+        },
+        ".tittle-1-mobile": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "22px",
+          color: "var(--color-black)",
+          fontWeight: "800",
+          lineHeight: "26.82px",
+        },
+        ".tittle-2": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "20px",
+          color: "var(--color-black)",
+          fontWeight: "800",
+          lineHeight: "30px",
+        },
+        ".tittle-2-mobile": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "16px",
+          color: "var(--color-black)",
+          fontWeight: "800",
+          lineHeight: "26.82px",
+        },
+        ".tittle-3-": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "16px",
+          color: "var(--color-black)",
+          fontWeight: "800",
+          lineHeight: "30px",
+        },
+        ".label": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "16px",
+          color: "var(--color-primary)",
+          fontWeight: "800",
+          lineHeight: "19.05px",
+        },
+        ".label-mobile": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "14px",
+          color: "var(--color-primary)",
+          fontWeight: "800",
+          lineHeight: "17.07px",
+        },
+        ".opaque-text": {
+          fontFamily: "var(--font-primary)",
+          opacity: "0.5",
+          fontSize: "14px",
+          color: "var(--color-primary)",
+          fontWeight: "600",
+          lineHeight: "17.07px",
+        },
+        ".menu-text": {
+          fontFamily: "var(--font-primary)",
+          fontSize: "16px",
+          color: "var(--color-primary)",
+          fontWeight: "600",
+          lineHeight: "19.05px",
+        },
+        ".paragraph-text": {
+          fontFamily: "var(--font-primary)",
+          opacity: "0.8",
+          fontSize: "16px",
+          color: "var(--color-primary)",
+          fontWeight: "600",
+          lineHeight: "19.05px",
+        },
+      });
+    }),
+  ],
 };
