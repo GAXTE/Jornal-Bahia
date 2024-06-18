ac8dc6abd0da10ae1c6c9cc0bb4eb2b25412348c
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-18 10:37:22 -0300

    feat: adding provider from context Post

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,18 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { CategoryProvider } from "./Providers/category/CategoryContext.tsx";
 import { PublicityProvider } from "./Providers/publicity/PublicityContext.tsx";
+import { PostProvider } from "./Providers/post/PostContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <CategoryProvider>
-        <PublicityProvider>
-          <App />
-        </PublicityProvider>
-      </CategoryProvider>
+      <PostProvider>
+        <CategoryProvider>
+          <PublicityProvider>
+            <App />
+          </PublicityProvider>
+        </CategoryProvider>
+      </PostProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
