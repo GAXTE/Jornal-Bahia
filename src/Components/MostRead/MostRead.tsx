840c49461c04cc4f1e87c14d69f17b3b3efd53fc
DanielVrs <danielvrsdev@gmail.com>
2024-06-18 13:27:10 -0300

    feat: change classename

--- src/Components/MostRead/MostRead.tsx
+++ src/Components/MostRead/MostRead.tsx
@@ -7,12 +7,16 @@ export const MostRead = () => {
   }
   return (
     <>
-      <div className=" max-w-[600px] mt-[57px]">
+      <div className=" max-w-[600px] ">
         <h2 className="mb-[26px] title-1">Mais lidos</h2>
         <ul className="flex flex-col gap-[25px] mb-12 ">
           {postMostState.slice(0, 3).map((post) => (
             <li key={post.id} className="flex gap-[41px] items-center">
-              <img src={post.photoUrls[0]} alt={"texto alternativo"} />
+              <img
+                className="max-w-[249px]"
+                src={post.photoUrls[0]}
+                alt={"texto alternativo"}
+              />
               <div className="flex flex-col gap-[6px]">
                 <strong>{post.categories[0].name}</strong>
                 <h3 className="text-lg">{post.title}</h3>
