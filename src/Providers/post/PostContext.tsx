0ac02332414a9a65792f089f495e40e8a33712f6
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-18 10:37:40 -0300

    feat: adding function for get Post Most Reads

--- src/Providers/post/PostContext.tsx
+++ src/Providers/post/PostContext.tsx
@@ -1,15 +1,40 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { IPost } from "../../types/PostTypes";
+import { Api } from "../../Services/api";
 
 interface Props {
   children: React.ReactNode;
 }
 
-interface IPostContext {}
+interface IPostContext {
+  getMostRead: () => void;
+  postMostState?: IPost[];
+}
 
-export const PostContext = React.createContext<IPostContext>({});
+export const PostContext = React.createContext<IPostContext>({
+  getMostRead: () => {},
+  postMostState: [],
+});
 
 export const PostProvider: React.FC<Props> = ({ children }) => {
-  return <PostContext.Provider value={{}}>{children}</PostContext.Provider>;
+  const [postMostState, setPostMostState] = useState<IPost[]>();
+
+  const postsMostReads = sessionStorage.getItem("postsMostReads");
+  const getMostRead = async () => {
+    if (postsMostReads) {
+      setPostMostState(JSON.parse(postsMostReads));
+      return;
+    }
+    try {
+      const { data } = await Api.get("/filter/post/views");
+      sessionStorage.setItem("postsMostReads", JSON.stringify(data));
+      setPostMostState(data);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getMostRead();
+  }, []);
+  return <PostContext.Provider value={{ postMostState, getMostRead }}>{children}</PostContext.Provider>;
 };
 
 export const usePostContext = () => useContext(PostContext);
