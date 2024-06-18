7c1247826c0c10977d0864c09d57419b6c55b789
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-18 15:03:14 -0300

    feat: adding logic to render just 5 random categories

--- src/Components/PostCategories/PostCatories.tsx
+++ src/Components/PostCategories/PostCatories.tsx
@@ -1,52 +1,62 @@
-import eagle from "../../assets/Moks/eagle.png";
+import React, { useEffect, useState } from "react";
+import { usePostContext } from "../../Providers/post/PostContext";
+import { IPost } from "../../types/PostTypes";
 
 export const PostCategories = () => {
+  const { AllPosts } = usePostContext();
+  const [uniqueCategoryPosts, setUniqueCategoryPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    if (AllPosts) {
+      const filteredPosts = filterUniquePosts(AllPosts);
+      const shuffledPosts = shuffleArray(filteredPosts);
+      const limitedPosts = shuffledPosts.slice(0, 5);
+      setUniqueCategoryPosts(limitedPosts);
+    }
+  }, [AllPosts]);
+
+  const filterUniquePosts = (posts: IPost[]): IPost[] => {
+    const uniqueCategoriesMap: { [categoryId: string]: IPost } = {};
+    posts.forEach((post) => {
+      if (post.categories && post.categories.length > 0) {
+        const categoryId = post.categories[0].id;
+        if (!uniqueCategoriesMap[categoryId]) {
+          uniqueCategoriesMap[categoryId] = post;
+        }
+      }
+    });
+    return Object.values(uniqueCategoriesMap);
+  };
+
+  const shuffleArray = (array: any[]): any[] => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
+  if (!AllPosts) return null;
+
   return (
-    <>
-      <div className="flex-col">
-        <span>categoria</span>
-        <div className="flex flex-col">
-          <img className="max-w-[348px] max-h-[188px]" src={eagle} alt="" />
-          <h2>
-            Notícia bombástica sobre um assunto polêmico que viralizou no TikTok
-          </h2>
-          <span>há 2 horas</span>
-        </div>
-        <ul>
-          <li>
-            <strong>categoria</strong>
-            <h3>
-              Notícia bombástica sobre um assunto polêmico que viralizou no
-              TikTok
-            </h3>
-            <span>há 2 horas</span>
-          </li>
-          <li>
-            <strong>categoria</strong>
-            <h3>
-              Notícia bombástica sobre um assunto polêmico que viralizou no
-              TikTok
-            </h3>
-            <span>há 2 horas</span>
-          </li>
-          <li>
-            <strong>categoria</strong>
-            <h3>
-              Notícia bombástica sobre um assunto polêmico que viralizou no
-              TikTok
-            </h3>
-            <span>há 2 horas</span>
-          </li>
-          <li>
-            <strong>categoria</strong>
-            <h3>
-              Notícia bombástica sobre um assunto polêmico que viralizou no
-              TikTok
-            </h3>
-            <span>há 2 horas</span>
-          </li>
-        </ul>
-      </div>
-    </>
+    <div className="flex-col">
+      {uniqueCategoryPosts.map((post, index) => (
+        <li key={post.id} className="flex flex-col mb-4">
+          <div className="flex flex-col mb-2">
+            {index === 0 && (
+              <img className="max-w-[348px] max-h-[188px]" src={post.photoUrls} alt={post.title} />
+            )}
+            <h2>{post.title}</h2>
+            <span>
+              {new Date(post.createdAt).toLocaleTimeString("pt-BR", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </span>
+            {index !== 0 && <strong>{post.categories[0].name}</strong>}
+          </div>
+        </li>
+      ))}
+    </div>
   );
 };
