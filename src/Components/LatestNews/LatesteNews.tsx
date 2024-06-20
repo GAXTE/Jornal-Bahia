7c009f4196272d3fa416573fb5d5a9a196a76301
DanielVrs <danielvrsdev@gmail.com>
2024-06-20 15:36:01 -0300

    fix: style components

--- src/Components/LatestNews/LatesteNews.tsx
+++ src/Components/LatestNews/LatesteNews.tsx
@@ -19,19 +19,26 @@ export const LatestNews = () => {
 
   return (
     <>
-      <h2>Latest News</h2>
-      <ul>
+      <ul className="flex flex-col gap-[25px] lg:max-w-[600px] ">
+        <h2 className="tittle-1-mobile lg:title-1">Últimas Notícias</h2>
         {paginatedView.map((post, index) => {
           idCount[post.id] = (idCount[post.id] || 0) + 1;
           const key = `${post.id}-${idCount[post.id]}`;
           return (
             <React.Fragment key={key}>
-              <li>
-                <img src={post.photoUrls[0]} alt={"texto alternativo"} />
-                <div>
-                  <strong>{post.categories[0].name}</strong>
-                  <h3>{post.title}</h3>
+              <li className="flex flex-col gap-[20px]">
+                <img
+                  className="max-w-[348px] max-h-[188px] rounded-lg lg:max-w-[249px] lg:min-w-[249px] lg:max-h-[249px] lg:min-h-[249px] object-cover"
+                  src={post.photoUrls[0]}
+                  alt={"texto alternativo"}
+                />
+                <div className="flex flex-col gap-3">
+                  <strong className="label-mobile lg:label">
+                    {post.categories[0].name}
+                  </strong>
+                  <h3 className="tittle-2-mobile lg:tittle-2">{post.title}</h3>
                 </div>
+                <span className="opaque-text mt-4">há 2 horas</span>
               </li>
               {(index + 1) % 3 === 0 && <PublicityBanner />}
             </React.Fragment>
--- src/Components/PostsMain/PostsMain.tsx
+++ src/Components/PostsMain/PostsMain.tsx
@@ -27,33 +27,33 @@ export const PostsMain = () => {
   }, []);
   return (
     <>
-      <section className="mt-[16px] mx-auto flex flex-wrap gap-[30px] lg:mt-[37px]">
+      <section className="mt-[16px] mx-auto flex flex-wrap gap-[30px] lg:mt-[37px] lg:justify-between">
         <div className="max-w-[600px] min-w-[320px]">
           <Slider />
         </div>
-        <section className="w-full flex-col max-w-[443px] min-w-[320px] max-h-[499px]">
-          {posts.map((post, index) => (
-            <div className="flex gap-4 mb-[20px]" key={index}>
-              <div className=" min-w-[79px] max-w-[153px]">
-                <img
-                  src={post.photoUrls[0]}
-                  alt=""
-                  className="rounded-md min-h-[100px] min-w-[79px] lg:h-[153px] lg:w-[153px] object-cover"
-                />
-              </div>
-              <div className="flex-col max-w-[263px] min-w-[200px]">
-                <span className="text-[#ff0000] mb-3">
-                  <strong>{post.categories[0].name}</strong>
-                </span>
-                <p className="overflow-auto whitespace-normal mb-1">
-                  <strong>{post.title}</strong>
-                </p>
-                <span className="text-sm opacity-50 text-black mt-2">
-                  há 2 horas
-                </span>
-              </div>
-            </div>
-          ))}
+        <section className="flex-col max-w-[443px] min-w-[320px] max-h-[499px]">
+          <ul className="">
+            {posts.map((post, index) => (
+              <li className="flex gap-4 mb-[20px] w-full" key={index}>
+                <div className=" min-w-[79px] max-w-[153px]">
+                  <img
+                    src={post.photoUrls[0]}
+                    alt=""
+                    className="rounded-lg min-h-[79px] max-w-[79px] lg:h-[153px] lg:w-[153px] lg:max-w-[153px]  object-cover"
+                  />
+                </div>
+                <div className="flex flex-col max-w-[263px] min-w-[200px]">
+                  <strong className=" label-mobile lg:label mb-2">
+                    {post.categories[0].name}
+                  </strong>
+                  <h3 className="tittle-3- overflow-auto whitespace-normal">
+                    {post.title}
+                  </h3>
+                  <span className="opaque-text mt-[6px]">há 2 horas</span>
+                </div>
+              </li>
+            ))}
+          </ul>
         </section>
       </section>
     </>
