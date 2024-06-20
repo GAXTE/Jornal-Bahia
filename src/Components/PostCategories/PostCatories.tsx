98ff6654c7e4ca58f3a05df0a522af587393da43
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-20 08:44:32 -0300

    fix: remove import react

--- src/Components/PostCategories/PostCatories.tsx
+++ src/Components/PostCategories/PostCatories.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { usePostContext } from "../../Providers/post/PostContext";
 import { IPost } from "../../types/PostTypes";
 
