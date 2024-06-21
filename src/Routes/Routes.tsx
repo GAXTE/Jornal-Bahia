9e1919d4bd434f7f7f232302df8f2bb5c4f56899
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-21 09:43:46 -0300

    feat: adding route for list post for categories

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Homepage } from "../Pages/HomePage/Homepage";
+import { PostCategoriesPage } from "../Pages/PostCategoriesPage/PostCategoriesPage";
 
 export const RouterMain = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/categories/:categorieId" element={<PostCategoriesPage />} />
       </Routes>
     </>
   );
