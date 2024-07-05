5cd5925e7c6ee38d15a6029802b618564b38718e
DanielVrs <danielvrsdev@gmail.com>
2024-07-05 10:32:05 -0300

    Fix: all final settings home page style

--- src/Components/PostsMain/PostsMain.tsx
+++ src/Components/PostsMain/PostsMain.tsx
@@ -31,14 +31,21 @@ export const PostsMain = () => {
   const handlePostClick = (postId: string) => {
     navi(`/viewpost/${postId}`);
   };
+
+  const MAX_CHARS = 70;
+
+  function truncateText(text: string, maxChars: number): string {
+    return text.length > maxChars ? `${text.substring(0, maxChars)}...` : text;
+  }
+
   return (
     <>
-      <section className="mt-[16px] mx-auto flex flex-col lg:flex-row items-center  gap-[30px] lg:mt-[37px] lg:justify-between">
+      <section className="mt-[16px] mx-auto flex flex-col lg:flex-row md:items-center  lg:items-stretch  gap-[30px] lg:mt-[37px] lg:justify-between">
         <div className="max-w-[600px] min-w-[320px]">
           <Slider />
         </div>
         <section className="flex-col min-w-[320px] md:w-[600px] lg:w-[430px]">
-          <ul className=" ">
+          <ul className="">
             {posts.map((post, index) => (
               <li className="flex gap-4 mb-[20px]" key={index}>
                 <div className=" min-w-[79px] max-w-[153px]">
@@ -50,12 +57,14 @@ export const PostsMain = () => {
                   />
                 </div>
                 <div className="flex flex-col max-w-[263px] min-w-[200px] md:min-w-[500px] lg:min-w-[100px] lg:max-w-[263px]">
-                  <strong className=" label-mobile lg:label mb-2">{post.categories[0].name}</strong>
+                  <strong className=" label-mobile lg:label mb-2">
+                    {post.categories[0].name}
+                  </strong>
                   <h3
                     className="cursor-pointer tittle-3- overflow-auto whitespace-normal "
                     onClick={() => handlePostClick(post.id)}
                   >
-                    {post.title}
+                    {truncateText(post.title, MAX_CHARS)}
                   </h3>
                   <DateComponent data={post.createdAt} />
                 </div>
