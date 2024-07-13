02916562962218f507160553cc84118f6ba6fe3a
Josenilsonfariasx <fariaslwork@gmail.com>
2024-07-13 01:09:50 -0300

    feat: adding page mostread

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,6 +3,7 @@ import { Homepage } from "../Pages/HomePage/Homepage";
 import { PostCategoriesPage } from "../Pages/PostCategoriesPage/PostCategoriesPage";
 import { SearchPage } from "../Pages/SearchPage/SearchPage";
 import { PostViewPage } from "../Pages/PostViewPage/PostViewPage";
+import { MostReadPage } from "../Pages/MostReadPage/MostReadPage";
 
 export const RouterMain = () => {
   return (
@@ -12,6 +13,7 @@ export const RouterMain = () => {
         <Route path="/categories/:categorieId" element={<PostCategoriesPage />} />
         <Route path="/search/:searchPost" element={<SearchPage />} />
         <Route path="/viewpost/:postId" element={<PostViewPage />} />
+        <Route path="/mostread" element={<MostReadPage />} />
       </Routes>
     </>
   );
