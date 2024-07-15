134635f2ff1ca0a01a0ce3f3f4bcab7deb96470f
Josenilsonfariasx <fariaslwork@gmail.com>
2024-07-15 09:11:05 -0300

    fix: banner cursor pointer

--- src/Components/PublicityBanner/PublicityBanner.tsx
+++ src/Components/PublicityBanner/PublicityBanner.tsx
@@ -2,16 +2,13 @@ import { useState, useEffect } from "react";
 import { usePublicityContext } from "../../Providers/publicity/PublicityContext";
 
 export const PublicityBanner = () => {
-  const { publicityBanners, getAllPublicity: getAllPublicty } =
-    usePublicityContext();
+  const { publicityBanners, getAllPublicity: getAllPublicty } = usePublicityContext();
   const [currentBannerIndex, setCurrentBannerIndex] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(async () => {
       await getAllPublicty();
-      setCurrentBannerIndex(
-        (prevIndex) => (prevIndex + 1) % publicityBanners.length
-      );
+      setCurrentBannerIndex((prevIndex) => (prevIndex + 1) % publicityBanners.length);
     }, 10000);
 
     return () => clearInterval(intervalId);
@@ -22,17 +19,13 @@ export const PublicityBanner = () => {
   }
 
   return (
-    <section className="cursor-pointer container flex justify-center items-center mt-[16px] lg:mt-[37px]">
+    <section className=" container flex justify-center items-center mt-[16px] lg:mt-[37px]">
       <div>
-        <a
-          href={publicityBanners[currentBannerIndex].link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href={publicityBanners[currentBannerIndex].link} target="_blank" rel="noopener noreferrer">
           <img
             src={publicityBanners[currentBannerIndex].imageUrl}
             alt={publicityBanners[currentBannerIndex].description}
-            className="h-[43px] w-[348px] md:w-[600px] max-w-full lg:h-[90px] lg:w-[728px] "
+            className="h-[43px] w-[348px] md:w-[600px] max-w-full lg:h-[90px] lg:w-[728px] cursor-pointer"
           />
         </a>
       </div>
